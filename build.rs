d333ed722ad7c44a4a3664b19b4c6a776cb3b072
Moritz Baron <r.fuzzo@gmail.com>
2023-11-21 09:05:10 +0100

    initial commit

---
+++ build.rs
@@ -0,0 +1,28 @@
+extern crate cmake;
+use cmake::Config;
+
+// print build script logs
+macro_rules! p {
+    ($($tokens: tt)*) => {
+        println!("cargo:warning={}", format!($($tokens)*))
+    }
+}
+
+fn main() {
+    let dst = Config::new("kraken").build_target("kraken_static").build();
+
+    // info
+    let profile = std::env::var("PROFILE").unwrap();
+    p!("PROFILE : {}", profile);
+    if profile == "debug" {
+        p!("DST: {}", dst.display());
+    }
+
+    // link
+    println!(
+        "cargo:rustc-link-search=native={}/build/bin/CMake/{}",
+        dst.display(),
+        profile
+    );
+    println!("cargo:rustc-link-lib=static=kraken_static");
+}
