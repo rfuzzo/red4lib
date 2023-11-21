84480e596e683736b77bf400351b5189652ffcad
Moritz Baron <r.fuzzo@gmail.com>
2023-11-21 09:40:13 +0100

    fix build script

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
-extern crate cmake;
 use cmake::Config;
+use std::{fs::File, path::PathBuf};
 
 // print build script logs
 macro_rules! p {
@@ -25,4 +25,15 @@ fn main() {
         profile
     );
     println!("cargo:rustc-link-lib=static=kraken_static");
+
+    // extract resources
+    let file_path = PathBuf::from("src/metadata-resources.csv");
+    if file_path.exists() {
+        p!("file exists: {}", file_path.display());
+    } else {
+        p!("extracting file: {}", file_path.display());
+        let f = File::open("src/metadata-resources.zip").expect("failed to open resource file");
+        let mut zip = zip::ZipArchive::new(f).expect("fialed to open zip file");
+        zip.extract("src/").expect("failed to extract zip file");
+    }
 }
